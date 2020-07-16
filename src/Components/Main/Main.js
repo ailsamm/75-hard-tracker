6c94b4abe0a440b8aaeb4fca717d85ffb41a1318
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-07-16 16:24:02 -0700

    Add router transition animation

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 import Tracker from '../Tracker/Tracker';
 import Logger from '../Logger/Logger';
 import './Main.css';
@@ -9,6 +10,12 @@ export default class Main extends Component {
         return (
             <main className="main">
                 <Switch>
+                    <AnimatedSwitch
+                        atEnter={{ opacity: 0 }}
+                        atLeave={{ opacity: 0 }}
+                        atActive={{ opacity: 1 }}
+                        className="switch-wrapper"
+                    >
                     <Route 
                         exact 
                         key='tracker'
@@ -20,6 +27,7 @@ export default class Main extends Component {
                         path='/logger/:id' 
                         component={Logger}
                     />
+                    </AnimatedSwitch>
                 </Switch>
             </main>
         )
