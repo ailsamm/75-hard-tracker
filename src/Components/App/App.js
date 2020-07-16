1818af3a08db2d73a8e2df6b3211a236fde9ca6e
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-07-16 07:17:21 -0700

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="app">
+              <header className="app__header">
+                <p>
+                  Hello world!
+                </p>
+              </header>
+            </div>
+        );
+    }
+}
+  
