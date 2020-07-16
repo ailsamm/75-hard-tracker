0c648f3d395a2d73195f8af03d3bb9ff493f0594
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-07-16 08:21:52 -0700

    Update context/dummy store

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Tracker from '../Tracker/Tracker';
+import AppContext from '../../AppContext';
+import STORE from '../../STORE';
 import './App.css';
 
 export default class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+          loggedInUser: 1,
+          users: [],
+          userLogs: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            users: STORE.users,
+            userLogs: STORE.userLogs
+        })
+    }
+
     render() {
+        const contextValue = {
+            users: this.state.users,
+            userLogs: this.state.userLogs
+        }
         return (
-            <div className="app">
-              <Header/>
-              <Tracker/>
-            </div>
+            <AppContext.Provider value={contextValue}>
+                <div className="app">
+                <Header/>
+                <Tracker/>
+                </div>
+            </AppContext.Provider>
         );
     }
 }
--- src/STORE.js
+++ src/STORE.js
@@ -1,7 +1,11 @@
-const users = [
-    {"user_id": 1, "user_name": "Ailsa"}
-]
+const STORE = {
+    users: [
+        {user_id: 1, user_name: "Ailsa"}
+    ],
+    userLogs: [
+        {user_id: 1, day_number: 1, logged: true, complete: true, workout1: true, workout2: true, water: true, no_alcohol: true, diet: true, read: true}
+    ]
+}
 
-const userLogs = [
-    {"user_id": 1, "day_number": 1, "logged": true}
-]
+
+export default STORE;
